fe2781cb57fed76e330083c97642fa28772f5828
Daniel Yankiver <dyankiver@gmail.com>
2020-11-04 16:56:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,9 @@ end
 def update_counting_hash(hash, key)
   hash[key] = "Hello"
   if hash[key]
-    hash[key] += 1
+    hash[key] = 1
   else 
-    hash[key] + 1 
+    hash[key] += 1 
   end
 end
 
